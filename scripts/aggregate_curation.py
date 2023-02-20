173c93683cb7f83e0e9793c77570693242a1332f
C-user for alavi <Calavi@phaedra.molgen.mpg.de>
2023-02-20 16:33:59 +0100

    script to aggregate curation results and do consensus voting

---
+++ scripts/aggregate_curation.py
@@ -0,0 +1,60 @@
+import sys 
+import pandas as pd 
+from glob import glob
+
+
+def consensus_curate(votes):
+    """ Performs sort of majority voting on the curation votes.
+    
+    param votes: A pandas series of votes. 1 for a positive vote, 0 for a negative vote, and NaN for no vote. 
+    
+    return 1 if the majority of votes are positive, 0 otherwise. """
+    
+    num_votes = votes.notna().sum()
+    if votes.sum() >= num_votes / 2:
+        return 1
+    else:
+        return 0
+
+
+def aggregate_curation(sample, date, directory, curators):
+    """ Aggregates the curation votes for each chunk and does consensus voting.
+
+    param sample: The sample name.
+    param date: The date of the curation.
+    param directory: The directory where the curation votes are stored.
+    param curators: The list of curators. """
+
+    curation_dfs = []
+    curator_cols = []
+    for curator in curators:
+        curator_col = 'Confirmed (' + curator + ')'
+        try:
+            curation_df = pd.concat([pd.read_csv(fname, sep='\t') for fname in glob(directory + '/*/*/*/' + date + '*' + curator + '_curated.tsv')], ignore_index=True)
+            curation_df = curation_df[['sample', 'tech', 'method', 'id', 'type', 'confirmed', 'pred_dicast', 'err_type', curator_col]].copy()
+            curation_df[curator_col] = curation_df[curator_col].replace({True: 1, False: 0})
+            curation_dfs.append(curation_df)
+            curator_cols.append(curator_col)
+        except ValueError:
+            pass
+
+    curation_df = curation_dfs[0]
+    if len(curation_dfs) > 1:
+        for i in range(1, len(curation_dfs)):
+            curation_df = curation_df.merge(curation_dfs[i], on=['sample', 'tech', 'method', 'id', 'type', 'confirmed', 'pred_dicast', 'err_type'], how='left')
+        curation_df['Confirmed (Consensus)'] = curation_df[curator_cols].apply(consensus_curate, axis=1)
+    else:
+        curation_df['Confirmed (Consensus)'] = curation_df[curator_cols[0]]
+    curation_df.to_csv(directory + '/' + date + '_' + sample + '_curated.tsv', sep='\t', index=False)
+
+
+date = sys.argv[1]
+ref = 'hg38'
+samples = ['17-08', '146-97', '176-98', '906-02', '18-5047']
+samples = ['146-97', '176-98', '906-02', '18-5047']
+curators = ['Nico', 'Uira', 'Jakob', 'Hossein', 'Maryam']
+directory = '/confidential/FamilyR13/DATA/10x/sv_compare/results/SAMPLE_REF/curation'
+
+for sample in samples:
+    aggregate_curation(sample, date, directory.replace('SAMPLE', sample).replace('REF', ref), curators)
+
